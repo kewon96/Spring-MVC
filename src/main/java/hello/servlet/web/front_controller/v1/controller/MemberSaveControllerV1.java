7a50a1f4c7f315b14d8ce2243a98ef2a30f03083
Kewon <yang0184@naver.com>
2022-04-12 23:12:09 +0900

    v1. FrontController

---
+++ src/main/java/hello/servlet/web/front_controller/v1/controller/MemberSaveControllerV1.java
@@ -0,0 +1,37 @@
+package hello.servlet.web.front_controller.v1.controller;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+import hello.servlet.web.front_controller.v1.ControllerV1;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MemberSaveControllerV1 implements ControllerV1 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+
+        Member member = Member.builder()
+                .username(username)
+                .age(age)
+                .build();
+
+        memberRepository.save(member);
+
+        // Model에 데이터 이식
+        request.setAttribute("member", member);
+
+        String viewPath = "/WEB-INF/views/save-result.jsp";
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher(viewPath);
+        requestDispatcher.forward(request, response);
+    }
+
+}
